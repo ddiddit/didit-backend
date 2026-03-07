82a7c760de94fb9f6ddbf864c3964c1f3e7b72ca
Kimdonghwan <kimdonghwan913@gmail.com>
2026-03-07 21:29:22 +0900

    style: BaseEntity ktlint 포맷 수정

--- src/main/kotlin/com/didit/domain/shared/BaseEntity.kt
+++ src/main/kotlin/com/didit/domain/shared/BaseEntity.kt
@@ -10,11 +10,14 @@ import java.time.LocalDateTime
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
-abstract class BaseEntity(
+abstract class BaseEntity {
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    var createdAt: LocalDateTime? = null,
+    var createdAt: LocalDateTime? = null
+        protected set
+
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    var updatedAt: LocalDateTime? = null,
-)
+    var updatedAt: LocalDateTime? = null
+        protected set
+}
