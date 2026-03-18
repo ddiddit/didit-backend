f2c4097be7640b7bd0d1f3fd313182b8149d64ac
hajimeong <ha_01@naver.com>
2026-03-18 16:45:54 +0900

    refactor: security 설정 정리 및 의존성 스타터 기반으로 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,11 +33,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
--- src/main/kotlin/com/didit/adapter/config/SecurityConfig.kt
+++ src/main/kotlin/com/didit/adapter/config/SecurityConfig.kt
@@ -2,7 +2,6 @@ package com.didit.adapter.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
@@ -15,13 +14,10 @@ class SecurityConfig {
             .csrf { it.disable() }
             .formLogin { it.disable() }
             .httpBasic { it.disable() }
-            .cors {}
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }.authorizeHttpRequests {
                 it
-                    .requestMatchers(HttpMethod.OPTIONS, "/**")
-                    .permitAll()
                     .requestMatchers(
                         "/auth/**",
                         "/health",
