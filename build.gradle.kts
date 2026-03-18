3c11784b7eb9ef861bcfc21a15deb9e6943c84a5
Develop-KIM <kimdonghwan913@gmail.com>
2026-03-19 00:15:52 +0900

    feat: actuator 의존성 추가 및 애플리케이션 야믈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
