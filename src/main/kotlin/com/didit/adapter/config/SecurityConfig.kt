c52e1a02a5ce47b2b0e5fb611934c26d40f61cea
hajimeong <ha_01@naver.com>
2026-03-19 03:04:42 +0900

    feat: SecurityConfig에 JwtFilter 적용

--- src/main/kotlin/com/didit/adapter/config/SecurityConfig.kt
+++ src/main/kotlin/com/didit/adapter/config/SecurityConfig.kt
@@ -1,15 +1,22 @@
 package com.didit.adapter.config
 
+import com.didit.adapter.auth.jwt.JwtFilter
+import com.didit.adapter.auth.jwt.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtProvider: JwtProvider,
+) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        val jwtFilter = JwtFilter(jwtProvider)
+
         http
             .csrf { it.disable() }
             .formLogin { it.disable() }
@@ -24,7 +31,10 @@ class SecurityConfig {
                     ).permitAll()
                     .anyRequest()
                     .authenticated()
-            }
+            }.addFilterBefore(
+                jwtFilter,
+                UsernamePasswordAuthenticationFilter::class.java,
+            )
 
         return http.build()
     }
