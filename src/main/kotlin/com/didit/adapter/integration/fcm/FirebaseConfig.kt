443fb9f256abc3ac3ff5d06c11ad000100067ff0
Develop-KIM <kimdonghwan913@gmail.com>
2026-03-22 19:24:08 +0900

    feat: FirebaseConfig 초기화 설정 추가

---
+++ src/main/kotlin/com/didit/adapter/integration/fcm/FirebaseConfig.kt
@@ -0,0 +1,25 @@
+package com.didit.adapter.integration.fcm
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+
+@Configuration
+class FirebaseConfig {
+
+    @Bean
+    fun firebaseApp(): FirebaseApp {
+        if (FirebaseApp.getApps().isNotEmpty()) {
+            return FirebaseApp.getInstance()
+        }
+
+        val serviceAccount = ClassPathResource("firebase/firebase-service-account.json").inputStream
+
+        val options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build()
+
+        return FirebaseApp.initializeApp(options)
+    }
+}
