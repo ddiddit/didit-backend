351fbc5c8f64409a44c0e1a1a31546a5f8fefa30
Develop-KIM <kimdonghwan913@gmail.com>
2026-03-25 18:36:09 +0900

    Revert "[DIT-38] 알림 개별 읽음 구현 (#24)"
    This reverts commit 97a2cc8cd6c977afab264dfc7ead2d93ec0da1e3.

--- src/main/kotlin/com/didit/application/notification/exception/NotificationErrorCode.kt
+++ src/main/kotlin/com/didit/application/notification/exception/NotificationErrorCode.kt
@@ -8,5 +8,4 @@ enum class NotificationErrorCode(
     override val detail: String,
 ) : BaseErrorCode {
     NOTIFICATION_SETTING_NOT_FOUND(HttpStatus.NOT_FOUND, "알림 설정을 찾을 수 없습니다."),
-    NOTIFICATION_HISTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "알림 히스토리를 찾을 수 없습니다."),
 }
