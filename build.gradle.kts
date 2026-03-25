3fcf0e85c9d4b51ebbbb9374ce0e2a6245f01f69
Develop-KIM <kimdonghwan913@gmail.com>
2026-03-25 19:15:41 +0900

    chore: jwt 및 oauth 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,12 +40,15 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.google.firebase:firebase-admin:9.2.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
 
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     runtimeOnly("com.mysql:mysql-connector-j")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
