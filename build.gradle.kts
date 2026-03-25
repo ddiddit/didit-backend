c160175fac738109b0acd6f8651357f0382d831e
Develop-KIM <kimdonghwan913@gmail.com>
2026-03-25 19:05:17 +0900

    build: aop 및 security 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -46,16 +48,17 @@ dependencies {
 
     runtimeOnly("com.mysql:mysql-connector-j")
 
-    testRuntimeOnly("com.h2database:h2")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
-    testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
 
     "asciidoctorExt"("org.springframework.restdocs:spring-restdocs-asciidoctor")
 }
