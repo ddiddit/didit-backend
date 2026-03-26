f94f4e098a9eb65189b33748f9cbf5c56b4b0f0f
Kimdonghwan <kimdonghwan913@gmail.com>
2026-03-26 14:09:04 +0900

    feat: 초기 슈퍼 어드민 계정 생성

--- src/main/kotlin/com/didit/application/admin/required/AdminRepository.kt
+++ src/main/kotlin/com/didit/application/admin/required/AdminRepository.kt
@@ -1,10 +1,13 @@
 package com.didit.application.admin.required
 
 import com.didit.domain.admin.Admin
+import com.didit.domain.admin.AdminRole
 import org.springframework.data.repository.Repository
 import java.util.UUID
 
 interface AdminRepository : Repository<Admin, UUID> {
+    fun existsByRole(role: AdminRole): Boolean
+
     fun save(admin: Admin): Admin
 
     fun findById(id: UUID): Admin?
