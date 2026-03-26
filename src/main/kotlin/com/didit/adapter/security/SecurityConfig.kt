5555e72eabb55bd1f7409d23b2fb11a98d010aae
Kimdonghwan <kimdonghwan913@gmail.com>
2026-03-26 13:24:07 +0900

    chore: cors 설정

--- src/main/kotlin/com/didit/adapter/security/SecurityConfig.kt
+++ src/main/kotlin/com/didit/adapter/security/SecurityConfig.kt
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +20,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val adminJwtAuthenticationFilter: AdminJwtAuthenticationFilter,
+    private val corsConfigurationSource: CorsConfigurationSource,
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
+            .cors { it.configurationSource(corsConfigurationSource) }
             .csrf { it.disable() }
             .formLogin { it.disable() }
             .httpBasic { it.disable() }
