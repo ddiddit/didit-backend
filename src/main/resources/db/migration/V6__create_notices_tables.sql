291965c27e9558ba9c51034db3ee45c435aa2a33
hajimeong <ha_01@naver.com>
2026-03-26 21:24:56 +0900

    feat: Flyway로 notices 테이블 생성

---
+++ src/main/resources/db/migration/V6__create_notices_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE notices (
+    id BINARY(16) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'DRAFT',
+    send_push BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    deleted_at DATETIME(6) NULL,
+    PRIMARY KEY (id)
+);
