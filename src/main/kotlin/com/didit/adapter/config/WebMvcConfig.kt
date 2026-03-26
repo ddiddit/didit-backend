4510c9a92d59e706641987a00866015a8b860fe8
monkey <105265603+Develop-KIM@users.noreply.github.com>
2026-03-26 14:26:35 +0900

    fix: 어드민 MVC 컨피그 적용 (#51)

--- src/main/kotlin/com/didit/adapter/config/WebMvcConfig.kt
+++ src/main/kotlin/com/didit/adapter/config/WebMvcConfig.kt
@@ -1,5 +1,6 @@
 package com.didit.adapter.config
 
+import com.didit.adapter.webapi.admin.resolver.CurrentAdminIdResolver
 import com.didit.adapter.webapi.auth.resolver.CurrentUserIdResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
@@ -8,8 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebMvcConfig(
     private val currentUserIdResolver: CurrentUserIdResolver,
+    private val currentAdminIdResolver: CurrentAdminIdResolver,
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(currentUserIdResolver)
+        resolvers.add(currentAdminIdResolver)
     }
 }
