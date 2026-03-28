a36a75907b9787b6a2c6d7b71ade034705e62adf
hajimeong <ha_01@naver.com>
2026-03-28 20:17:47 +0900

    feat: 문의(inquiries) 테이블 생성 마이그레이션 추가

---
+++ src/main/resources/db/migration/V8__create_inquiries_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE inquiries (
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    user_id BINARY(16) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    type_etc VARCHAR(255) NULL,
+    content TEXT NOT NULL,
+    is_agreed BOOLEAN NOT NULL,
+    admin_answer TEXT NULL,
+    admin_id BINARY(16) NULL,
+    answered_at DATETIME(6) NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    deleted_at DATETIME(6) NULL
+)
