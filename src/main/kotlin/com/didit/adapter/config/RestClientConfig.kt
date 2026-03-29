75cca687405c0b960233bef5eedf3f17ca3ad229
heveidndksjd <kayoonkim@naver.com>
2026-03-29 23:25:01 +0900

    feat: stt 기능 구현

--- src/main/kotlin/com/didit/adapter/config/RestClientConfig.kt
+++ src/main/kotlin/com/didit/adapter/config/RestClientConfig.kt
@@ -1,9 +1,11 @@
 package com.didit.adapter.config
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
 
+@EnableConfigurationProperties(ClovaSpeechProperties::class)
 @Configuration
 class RestClientConfig {
     @Bean
