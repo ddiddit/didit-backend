1b7a3871ca96651dbe6411a926555d2da953b62a
Kimdonghwan <kimdonghwan913@gmail.com>
2026-04-02 16:16:31 +0900

    test: 모키토 임포트 추가

--- src/test/kotlin/com/didit/application/achievement/BadgeQueryServiceTest.kt
+++ src/test/kotlin/com/didit/application/achievement/BadgeQueryServiceTest.kt
@@ -11,6 +11,10 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.never
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import java.util.UUID
 
