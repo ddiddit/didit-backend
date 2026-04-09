86271bbf6fe752513ce264a68682aab2099b48a1
heveidndksjd <kayoonkim@naver.com>
2026-04-09 20:26:37 +0900

    refactor: 회고 요약 구조 수정

--- src/main/kotlin/com/didit/adapter/webapi/retrospect/dto/ContentResponse.kt
+++ src/main/kotlin/com/didit/adapter/webapi/retrospect/dto/ContentResponse.kt
@@ -2,11 +2,19 @@ package com.didit.adapter.webapi.retrospect.dto
 
 data class ContentResponse(
     val summary: String,
-    val feedback: String,
-    val insight: String,
-    val doneWork: String,
     val blockedPoint: List<String>,
     val solutionProcess: List<String>,
     val lessonLearned: List<String>,
-    val nextAction: List<String>,
+    val insight: InsightContentResponse,
+    val nextAction: NextActionContentResponse,
+)
+
+data class InsightContentResponse(
+    val title: String,
+    val description: String,
+)
+
+data class NextActionContentResponse(
+    val title: String,
+    val description: String,
 )
