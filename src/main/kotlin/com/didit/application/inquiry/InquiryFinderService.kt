3c928ec1a8c90ca7c48db6c0a39aef6bb0646ee8
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-06-27 00:41:57 +0900

    feat: 문의 내역 조회 1년 필터링 추가

--- src/main/kotlin/com/didit/application/inquiry/InquiryFinderService.kt
+++ src/main/kotlin/com/didit/application/inquiry/InquiryFinderService.kt
@@ -6,6 +6,7 @@ import com.didit.application.inquiry.required.InquiryRepository
 import com.didit.domain.inquiry.Inquiry
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Transactional(readOnly = true)
@@ -14,12 +15,16 @@ class InquiryFinderService(
     private val inquiryRepository: InquiryRepository,
 ) : InquiryFinder {
     override fun findAll(userId: UUID): List<Inquiry> {
-        val inquiries = inquiryRepository.findAllByUserIdAndDeletedAtIsNull(userId)
+        val inquiries =
+            inquiryRepository.findAllByUserIdAndDeletedAtIsNullAndCreatedAtAfterOrderByCreatedAtDesc(
+                userId,
+                LocalDateTime.now().minusYears(1),
+            )
 
         return inquiries
     }
 
-    override fun findAll(): List<Inquiry> {
+    override fun findAllForAdmin(): List<Inquiry> {
         val inquiries = inquiryRepository.findAllByDeletedAtIsNullOrderByCreatedAtDesc()
 
         return inquiries
